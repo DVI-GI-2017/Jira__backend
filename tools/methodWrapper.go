d624287adc6e9d438e3bbe119a07028fd75d5d4b
vladpereskokov <vladpereskokov@mail.ru>
2017-08-15 15:00:23 +0300

    change wrapper

--- tools/methodWrapper.go
+++ tools/methodWrapper.go
@@ -1,8 +1,10 @@
 package tools
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func GetOnly(h func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+func GetOnly(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			h(w, r)
@@ -13,3 +15,33 @@ func GetOnly(h func(w http.ResponseWriter, r *http.Request)) func(w http.Respons
 		http.Error(w, "Method not allowed! Get only", http.StatusMethodNotAllowed)
 	}
 }
+//
+//func BasicAuth(pass func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+//
+//	return func(w http.ResponseWriter, r *http.Request) {
+//
+//		auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+//
+//		if len(auth) != 2 || auth[0] != "Basic" {
+//			http.Error(w, "authorization failed", http.StatusUnauthorized)
+//			return
+//		}
+//
+//		payload, _ := base64.StdEncoding.DecodeString(auth[1])
+//		pair := strings.SplitN(string(payload), ":", 2)
+//
+//		if len(pair) != 2 || !validate(pair[0], pair[1]) {
+//			http.Error(w, "authorization failed", http.StatusUnauthorized)
+//			return
+//		}
+//
+//		pass(w, r)
+//	}
+//}
+//
+//func validate(username, password string) bool {
+//	if username == "test" && password == "test" {
+//		return true
+//	}
+//	return false
+//}
