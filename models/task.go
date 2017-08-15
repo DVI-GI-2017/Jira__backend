dc6e6ec2c528416da771067c21f898e333d64076
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-15 15:30:01 +0300

    Add initiator to task.

--- models/task.go
+++ models/task.go
@@ -3,6 +3,7 @@ package models
 type Task struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	Initiator   *User `json:"initiator"`
 	Assignee    *User `json:"assignee"`
 	Labels      Labels `json:"labels"`
 }
