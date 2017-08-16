c6a8de50e6bafd58e77538f8c77a52edb383a1a1
vladpereskokov <vladpereskokov@mail.ru>
2017-08-16 18:42:17 +0300

    add db handler

--- auth/credentials.go
+++ auth/credentials.go
@@ -1,8 +1,12 @@
 package auth
 
 import (
-	"github.com/DVI-GI-2017/Jira__backend/db"
 	"errors"
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/DVI-GI-2017/Jira__backend/models"
+	"github.com/DVI-GI-2017/Jira__backend/db"
+	"github.com/DVI-GI-2017/Jira__backend/configs"
 )
 
 type Credentials struct {
@@ -21,11 +25,22 @@ func LoginUser(user *Credentials) (err error) {
 	return errors.New("Invalid credentials")
 }
 
-func RegisterUser(user *Credentials) (error) {
-	for _, value := range db.FakeUsers {
-		if user.Email == value.Email {
-			return errors.New("User with this email already exists!")
-		}
-	}
-	return nil
+func RegisterUser(user *Credentials) (result models.User, err error) {
+	connection := db.NewDBConnection(configs.ConfigInfo.Mongo)
+	defer connection.CloseConnection()
+
+	users := connection.GetCollection(configs.ConfigInfo.Mongo)
+
+	result = models.User{}
+	err = users.Find(bson.M{
+		"$and": []interface{}{
+			bson.M{"email": user.Email},
+			bson.M{"password": user.Password},
+		},
+	}).One(&result)
+
+	fmt.Println("Result")
+	fmt.Println(result)
+
+	return
 }
--- handlers/login.go
+++ handlers/login.go
@@ -5,12 +5,8 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
-	"gopkg.in/mgo.v2/bson"
-	"github.com/DVI-GI-2017/Jira__backend/db"
 	"github.com/DVI-GI-2017/Jira__backend/tools"
 	"github.com/DVI-GI-2017/Jira__backend/auth"
-	"github.com/DVI-GI-2017/Jira__backend/models"
-	"github.com/DVI-GI-2017/Jira__backend/configs"
 )
 
 var RegisterUser = PostOnly(
@@ -26,30 +22,13 @@ var RegisterUser = PostOnly(
 			return
 		}
 
-		if err := auth.RegisterUser(&credentials); err != nil {
+		result, err := auth.RegisterUser(&credentials)
+		if err == nil {
 			w.WriteHeader(http.StatusConflict)
 
 			fmt.Fprint(w, "User with this email already exists.")
-			log.Printf("%v", err)
 
 			return
-		}
-
-		user := models.User{Email: credentials.Email, Password: credentials.Password}
-
-		db.FakeUsers = append(db.FakeUsers, user)
-		w.WriteHeader(http.StatusOK)
-
-		connection := db.NewDBConnection(configs.ConfigInfo.Mongo)
-		defer connection.CloseConnection()
-
-		users := connection.GetCollection(configs.ConfigInfo.Mongo)
-
-		result := models.User{}
-		err := users.Find(bson.M{"firstname": "Jeremy"}).Select(bson.M{"Email": 0}).One(&result)
-		if err != nil {
-			w.WriteHeader(404)
-			fmt.Fprint(w, "Bad find")
 		}
 
 		w.WriteHeader(http.StatusOK)
