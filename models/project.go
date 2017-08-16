d904aaad52bd2997552766b7912b71f3c4288a25
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-16 06:05:24 +0300

    Add timestamps to projects.

--- models/project.go
+++ models/project.go
@@ -1,12 +1,17 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"time"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Project struct {
 	Id          bson.ObjectId
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Tasks       Tasks `json:"tasks"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Projects []Project
