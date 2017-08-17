dd50412b9579be1313c5751f62b4500cd74bc198
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-18 00:36:48 +0300

    Add helper functions and update handlers params.

--- routes/router.go
+++ routes/router.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -41,8 +43,8 @@ func (r *router) SetRootPath(path string) error {
 }
 
 //Helper types for different http method handlers
-type GetHandlerFunc func(*http.ResponseWriter, GetParams, PathParams)
-type PostHandlerFunc func(*http.ResponseWriter, PostBody, PathParams)
+type GetHandlerFunc func(http.ResponseWriter, GetParams, PathParams)
+type PostHandlerFunc func(http.ResponseWriter, PostBody, PathParams)
 
 // Example: url "/api/v1/users/1" and pattern "/api/v1/users/:id"
 // path params = {"id": "1"}
@@ -50,16 +52,6 @@ type PathParams map[string][]byte
 
 // Get params stands for "query params"
 type GetParams map[string]string
-
-// Converts url.Url.Query() from "Values" (map[string][]string)
-// to "GetParams" (map[string]string)
-func valuesToGetParams(values url.Values) GetParams {
-	var params map[string]string
-	for key := range values {
-		params[key] = values.Get(key)
-	}
-	return params
-}
 
 // Type for http post body
 type PostBody []byte // Byte array with request body
@@ -79,24 +71,54 @@ func (r *router) Get(pattern string, handler GetHandlerFunc) error {
 func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	method := req.Method
 
+	relPath, err := relativePath(r.root.Path, req.URL.Path)
+	if err != nil {
+		http.NotFound(w, req)
+	}
+
 	switch method {
 
 	case http.MethodGet:
-		r.handleGet(w, req)
+		r.handleGet(w, relPath, valuesToGetParams(req.URL.Query()))
 	case http.MethodPost:
-		r.handlePost(w, req)
+		body, err := readToPostBody(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Panicf("invalid body: %v", err)
+		}
+
+		r.handlePost(w, relPath, body)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "method not allowed: %s", method)
 	}
 }
 
-func (r *router) handlePost(w http.ResponseWriter, req *http.Request) {
+// Converts url.Url.Query() from "Values" (map[string][]string)
+// to "GetParams" (map[string]string)
+func valuesToGetParams(values url.Values) GetParams {
+	var params map[string]string
+	for key := range values {
+		params[key] = values.Get(key)
+	}
+	return params
+}
+
+// Read from reader to byte buffer
+func readToPostBody(r io.ReadCloser) ([]byte, error) {
+	const capacity = 100
+	body := make([]byte, 0, capacity)
+
+	_, err := r.Read(body)
+
+	return body, err
+}
+
+func (r *router) handleGet(w http.ResponseWriter, path string, getParams GetParams) {
 
 }
 
-func (r *router) handleGet(w http.ResponseWriter, req *http.Request) {
-
+func (r *router) handlePost(w http.ResponseWriter, path string, body PostBody) {
 }
 
 func relativePath(base string, absolute string) (string, error) {
