600f90a60f1124e1f70ef6fc949532431914b5fa
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-17 22:14:51 +0300

    Add scratch for router structure.

--- routes/router.go
+++ routes/router.go
@@ -2,17 +2,9 @@ package routes
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"net/url"
 )
-
-type router struct {
-	root *url.URL
-
-	getHandlers  map[string]GetHandlerFunc
-	postHandlers map[string]PostHandlerFunc
-}
 
 func NewRouter(rootPath string) (*router, error) {
 	r := &router{}
@@ -25,6 +17,16 @@ func NewRouter(rootPath string) (*router, error) {
 	return r, nil
 }
 
+type Pattern string
+
+type router struct {
+	root *url.URL
+
+	getHandlers  map[Pattern]GetHandlerFunc
+	postHandlers map[Pattern]PostHandlerFunc
+}
+
+// Set router root path, other paths will be relative to it
 func (r *router) SetRootPath(path string) error {
 	newRoot, err := url.Parse(path)
 	if err != nil {
@@ -36,47 +38,61 @@ func (r *router) SetRootPath(path string) error {
 	return nil
 }
 
-func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-	method := req.Method
-
-	switch method {
-
-	case http.MethodGet:
-		if route, ok := r.getHandlers[path]; ok {
-			route(&w, valuesToGetParams(req.URL.Query()), nil)
-		}
-		http.NotFound(w, req)
-	case http.MethodPost:
-		if route, ok := r.postHandlers[path]; ok {
-			var body []byte
-			_, err := req.Body.Read(body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Panicf("can not read request body: %v", err)
-			}
-
-			route(&w, PostBody(body), nil)
-		}
-		http.NotFound(w, req)
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "method not allowed: %s", method)
-	}
-}
-
+//Helper types for different http method handlers
 type GetHandlerFunc func(*http.ResponseWriter, GetParams, PathParams)
 type PostHandlerFunc func(*http.ResponseWriter, PostBody, PathParams)
 
+// Example: url "/api/v1/users/1" and pattern "/api/v1/users/:id"
+// path params = {"id": "1"}
 type PathParams map[string][]byte
-type GetParams map[string]string
-type PostBody []byte // Byte array with request body
 
+// Get params stands for "query params"
+type GetParams map[string]string
+
+// Converts url.Url.Query() from "Values" (map[string][]string)
+// to "GetParams" (map[string]string)
 func valuesToGetParams(values url.Values) GetParams {
 	var params map[string]string
 	for key := range values {
 		params[key] = values.Get(key)
 	}
 	return params
+}
+
+// Type for http post body
+type PostBody []byte // Byte array with request body
+
+// Add new get handler
+func (r *router) Get(pattern string, handler GetHandlerFunc) error {
+	fullPattern, err := r.root.Parse(pattern)
+	if err != nil {
+		return err
+	}
+
+	r.getHandlers[Pattern(fullPattern.Path)] = handler
+
+	return nil
+}
+
+func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	method := req.Method
+
+	switch method {
+
+	case http.MethodGet:
+		r.handleGet(w, req)
+	case http.MethodPost:
+		r.handlePost(w, req)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "method not allowed: %s", method)
+	}
+}
+
+func (r *router) handlePost(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (r *router) handleGet(w http.ResponseWriter, req *http.Request) {
+
 }
