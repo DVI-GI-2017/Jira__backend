269816ec851dbb29e6fa197d655c2d81f0ff20c9
vladpereskokov <vladpereskokov@mail.ru>
2017-08-18 23:36:29 +0300

    add test worker

--- pool/workers.go
+++ pool/workers.go
@@ -1,9 +1,18 @@
 package pool
 
-import "runtime"
+import (
+	"Jira__backend/models"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"io"
+	"log"
+	"runtime"
+)
 
 type Job struct {
 	JobId int
+	Email string
 }
 
 type JobResult struct {
@@ -13,13 +22,68 @@ type JobResult struct {
 	ResultType interface{}
 }
 
-func InitWorkers()  {
+var queue = make(chan *Job, 512)
+var results = make(chan *JobResult, 512)
+
+func InitWorkers() {
 	for id := 0; id < runtime.NumCPU(); id++ {
 		go worker(id, queue, results)
 	}
 }
 
 func worker(id int, queue chan *Job, results chan<- *JobResult) {
+	var session *mgo.Session
+	users := connect(id, session)
 
+	for job := range queue {
+
+		// Perform the database query
+		err := users.Insert(models.User{
+			FirstName: fmt.Sprintf("User %d", "Vasya"),
+			Email:     fmt.Sprintf("user-%s@example.com", job.Email),
+		})
+
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			// Our job hasn't completed because the database is no longer connected
+			// Put our job back onto the queue (in another go routine to avoid blocking if queue buffer is full)
+			// Then reconnect the database and continue processing
+			go func(job *Job, queue chan *Job) {
+				queue <- job
+			}(job, queue)
+			users = connect(id, session)
+			continue
+		}
+
+		user := new(models.User)
+		userid := fmt.Sprintf("User %s", job.Email)
+
+		err = users.Find(bson.M{
+			"$and": []interface{}{
+				bson.M{"email": userid},
+			},
+		}).One(&user)
+
+		// Send our results back
+		results <- &JobResult{
+			JobId:      job.JobId,
+			WorkerId:   id,
+			Error:      err,
+			ResultType: user,
+		}
+	}
 }
 
+func connect(workerId int, session *mgo.Session) *mgo.Collection {
+	for {
+		// Open a DB connection
+		log.Printf("Worker %d: Connecting to %s", workerId, fmt.Sprintf("mongodb://%s/%s", *host, *db))
+		s, err := mgo.Dial("mongodb://localhost:27017/worker-test")
+		if err != nil {
+			log.Printf("Worker %d: Unable to connect to database (%s)", workerId, err)
+			continue
+		}
+
+		// Connect to the DB collection
+		return s.DB("worker-test").C("users")
+	}
+}
