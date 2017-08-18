91fb95b9683221d032051bf4d0f79eb422bb232d
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-18 15:36:24 +0300

    Add more verbose check when adding new route.

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,21 @@
 package routes
 
 import (
+	"log"
+
 	"github.com/DVI-GI-2017/Jira__backend/handlers"
 )
 
 func InitRouter(r *router) {
-	r.Post("/signup", handlers.RegisterUser)
-	r.Post("/signin", handlers.Login)
+	const signup = "/signup"
+	err := r.Post(signup, handlers.RegisterUser)
+	if err != nil {
+		log.Panicf("can not init route '%s': %v", signup, err)
+	}
+
+	const signin = "/signin"
+	err = r.Post(signin, handlers.Login)
+	if err != nil {
+		log.Panicf("can not init route '%s': %v", signin, err)
+	}
 }
