925251dbfb1679ea7860e4fafe6a16d9cdbe0ccd
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-19 13:13:25 +0300

    Add benchmarks for routers.

--- routes/router_test.go
+++ routes/router_test.go
@@ -4,6 +4,16 @@ import (
 	"reflect"
 	"regexp"
 	"testing"
+
+	"net/http"
+
+	"fmt"
+
+	"net/http/httptest"
+
+	"bytes"
+
+	"github.com/gorilla/mux"
 )
 
 func TestRouter_SetRootPath(t *testing.T) {
@@ -55,5 +65,49 @@ func TestSimplifiedPattern(t *testing.T) {
 
 	if !reflect.DeepEqual(pathParams, expectedPathParams) {
 		t.Fail()
+	}
+}
+
+func gorillaHandler(w http.ResponseWriter, r *http.Request) {
+	getParams := r.URL.Query()
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "get params: %v\npath params: %v", getParams, vars)
+}
+
+func BenchmarkGorilla(b *testing.B) {
+	// Create router
+	router := mux.NewRouter()
+	apiRouter := router.PathPrefix("/api/v1").Subrouter()
+	apiRouter.Path("/users/{id:[0-9]+}").Methods(http.MethodGet).HandlerFunc(gorillaHandler)
+
+	// Create request
+	reader := bytes.NewBufferString("")
+	request := httptest.NewRequest(http.MethodGet, "/api/v1/users/1", reader)
+	response := httptest.NewRecorder()
+
+	for i := 0; i < b.N; i++ {
+		router.ServeHTTP(response, request)
+	}
+}
+
+func customHandler(w http.ResponseWriter, getParams map[string]string, pathParams map[string]string) {
+	fmt.Fprintf(w, "get params: %v\npath params: %v", getParams, pathParams)
+}
+
+func BenchmarkCustom(b *testing.B) {
+	// Create router
+	router, err := NewRouter("/api/v1")
+	if err != nil {
+		b.Errorf("can not create router: %v", err)
+	}
+	router.Get("/users/:id", customHandler)
+
+	// Create request
+	reader := bytes.NewBufferString("")
+	request := httptest.NewRequest(http.MethodGet, "/api/v1/users/1", reader)
+	response := httptest.NewRecorder()
+
+	for i := 0; i < b.N; i++ {
+		router.ServeHTTP(response, request)
 	}
 }
