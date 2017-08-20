de68b89016cfac91281d7ff61ea8c10e55baa9fc
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 01:41:44 +0300

    Extract users collection name.

--- services/users/service.go
+++ services/users/service.go
@@ -2,14 +2,16 @@ package users
 
 import "github.com/DVI-GI-2017/Jira__backend/db"
 
+const collection = "Users"
+
 func Insert(mongo *db.MongoConnection, user interface{}) (result interface{}, err error) {
-	return mongo.Insert("users", user)
+	return mongo.Insert(collection, user)
 }
 
 func All(mongo *db.MongoConnection, _ interface{}) (result interface{}, err error) {
-	return mongo.FindAll("users")
+	return mongo.FindAll(collection)
 }
 
 func GetUserByEmailAndPassword(mongo *db.MongoConnection, user interface{}) (result interface{}, err error) {
-	return mongo.Find("users", user)
+	return mongo.Find(collection, user)
 }
