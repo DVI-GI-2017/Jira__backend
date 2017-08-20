ce4872eed66377de6c0d5ef5234c85fa3d2d9b7b
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 00:57:22 +0300

    Update model helpers names.

--- models/project.go
+++ models/project.go
@@ -2,16 +2,17 @@ package models
 
 import (
 	"time"
+
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Project struct {
 	Id          bson.ObjectId
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Tasks       Tasks `json:"tasks"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	Tasks       TasksList `json:"tasks"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
 }
 
-type Projects []Project
+type ProjectsList []Project
--- models/task.go
+++ models/task.go
@@ -2,18 +2,19 @@ package models
 
 import (
 	"time"
+
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Task struct {
 	Id          bson.ObjectId
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Initiator   *User `json:"initiator"`
-	Assignee    *User `json:"assignee"`
-	Labels      Labels `json:"labels"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Initiator   *User     `json:"initiator"`
+	Assignee    *User     `json:"assignee"`
+	Labels      LabelsList   `json:"labels"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type Tasks []Task
+type TasksList []Task
