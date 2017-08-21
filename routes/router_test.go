7a786d96a74c29f83e05e2f58c6b0b51a4000c6f
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 10:06:28 +0300

    Fix 25 elements to 24.

--- routes/router_test.go
+++ routes/router_test.go
@@ -150,13 +150,13 @@ func customHandler(w http.ResponseWriter, req *http.Request) {
 
 func processRequest(router http.Handler, b *testing.B) {
 	reader := bytes.NewBufferString("")
-	request := httptest.NewRequest(http.MethodGet, "/api/v1/users/1234feabc1357346781234524", reader)
+	request := httptest.NewRequest(http.MethodGet, "/api/v1/users/234feabc1357346781234524", reader)
 	response := httptest.NewRecorder()
 
 	router.ServeHTTP(response, request)
 
 	s := fmt.Sprintf("%s", response.Body)
-	expected := "get params: map[], path params: map[id:1234feabc1357346781234524]"
+	expected := "get params: map[], path params: map[id:234feabc1357346781234524]"
 	if s != expected {
 		b.Errorf("invalid response: %s; expected: %s", s, expected)
 	}
