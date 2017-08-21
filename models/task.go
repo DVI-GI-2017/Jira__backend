a7283eb41a8e5a83e41e613e73b28d7d818085b6
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 17:16:48 +0300

    Add handlers for tasks.

--- models/task.go
+++ models/task.go
@@ -9,12 +9,12 @@ import (
 type Task struct {
 	Id          bson.ObjectId `json:"_id" bson:"_id,omitempty"`
 	Title       string        `json:"title" bson:"title"`
-	Description string        `json:"description" bson:"description"`
-	Initiator   *User         `json:"initiator" bson:"initiator"`
-	Assignee    *User         `json:"assignee" bson:"assignee"`
-	Labels      LabelsList    `json:"labels" bson:"labels"`
-	CreatedAt   time.Time     `json:"created_at" bson:"created_at"`
-	UpdatedAt   time.Time     `json:"updated_at" bson:"updated_at"`
+	Description string        `json:"description" bson:"description,omitempty"`
+	Initiator   *User         `json:"initiator" bson:"initiator,omitempty"`
+	Assignee    *User         `json:"assignee" bson:"assignee,omitempty"`
+	Labels      LabelsList    `json:"labels" bson:"labels,omitempty"`
+	CreatedAt   time.Time     `json:"created_at" bson:"created_at,omitempty"`
+	UpdatedAt   time.Time     `json:"updated_at" bson:"updated_at,omitempty"`
 }
 
 type TasksList []Task
