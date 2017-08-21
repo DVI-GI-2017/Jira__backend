f1cb60f4d711a013a44351c6b2779ccb4c744513
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 23:18:06 +0300

    Simplify label type.

--- models/label.go
+++ models/label.go
@@ -1,10 +1,5 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
-
-type Label struct {
-	Id   bson.ObjectId `json:"_id" bson:"_id,omitempty"`
-	Name string        `json:"name" bson:"name"`
-}
+type Label string
 
 type LabelsList []Label
