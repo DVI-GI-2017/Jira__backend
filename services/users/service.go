c4bad84d477e1423baf319d0fe3092cd04602ee4
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-21 06:03:21 +0300

    Update users handler.

--- services/users/service.go
+++ services/users/service.go
@@ -22,8 +22,10 @@ func Insert(mongo *mgo.Database, user interface{}) (result interface{}, err erro
 	return user, mongo.C(collection).Insert(user)
 }
 
-func All(mongo *mgo.Database, _ interface{}) (result interface{}, err error) {
-	result = new(models.UsersList)
+func All(mongo *mgo.Database) (result models.UsersList, err error) {
+	const defaultSize = 100
+	result = make(models.UsersList, defaultSize)
+
 	err = mongo.C(collection).Find(bson.M{}).All(result)
 	return
 }
