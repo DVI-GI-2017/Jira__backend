2838560e960957914b928f52b41231ee7f01d0a1
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-23 23:47:20 +0300

    Add validation to task model.

--- models/task.go
+++ models/task.go
@@ -5,13 +5,36 @@ import (
 )
 
 type Task struct {
-	Id          AutoId `json:"_id" bson:"_id,omitempty"`
-	Title       Name        `json:"title" bson:"title"`
+	Id          AutoId     `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title       Name       `json:"title" bson:"title"`
 	ProjectId   RequiredId `json:"project_id" bson:"project_id"`
-	Description string        `json:"description" bson:"description,omitempty"`
-	InitiatorId bson.ObjectId `json:"initiator_id" bson:"initiator_id,omitempty"`
-	AssigneeId  bson.ObjectId `json:"assignee_id" bson:"assignee_id,omitempty"`
-	Labels      LabelsList    `json:"labels" bson:"labels,omitempty"`
+	Description Text       `json:"description,omitempty" bson:"description,omitempty"`
+	InitiatorId RequiredId `json:"initiator_id" bson:"initiator_id"`
+	AssigneeId  OptionalId `json:"assignee_id,omitempty" bson:"assignee_id,omitempty"`
+	Labels      LabelsList `json:"labels,omitempty" bson:"labels,omitempty"`
+}
+
+// Validates Task
+func (t Task) Validate() error {
+	if err := t.Id.Validate(); err != nil {
+		return err
+	}
+	if err := t.Title.Validate(); err != nil {
+		return err
+	}
+	if err := t.ProjectId.Validate(); err != nil {
+		return err
+	}
+	if err := t.Description.Validate(); err != nil {
+		return err
+	}
+	if err := t.InitiatorId.Validate(); err != nil {
+		return err
+	}
+	if err := t.AssigneeId.Validate(); err != nil {
+		return err
+	}
+	return nil
 }
 
 type TasksList []Task
