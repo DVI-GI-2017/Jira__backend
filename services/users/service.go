b817110e43724629f3c287dc779256ac34231ffc
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-24 20:42:31 +0300

    Fix missed return values in user projects.

--- services/users/service.go
+++ services/users/service.go
@@ -81,7 +81,7 @@ func AllUsersProject(source db.DataSource, id models.RequiredId) (projects model
 		return models.ProjectsList{}, fmt.Errorf("can not find user with id '%s': %v", id, err)
 	}
 
-	err = source.C(cProjects).Find(bson.M{"_id": user.Projects}).All(&projects)
+	err = source.C(cProjects).Find(bson.M{"_id": bson.M{"$in": user.Projects}}).All(&projects)
 	if err != nil {
 		return models.ProjectsList{}, fmt.Errorf("can not retrieve all users from project: %s", id.Hex())
 	}
