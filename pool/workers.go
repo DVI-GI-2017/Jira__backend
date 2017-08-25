837447aaa9556a7053c643b87270e567ca21a8c4
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-25 19:56:41 +0300

    Move almost all work with channels to workers file.

--- pool/workers.go
+++ pool/workers.go
@@ -2,8 +2,32 @@ package pool
 
 import (
 	"runtime"
+
+	"github.com/DVI-GI-2017/Jira__backend/db"
 )
 
+// Type and channel for jobs
+type job struct {
+	service ServiceFunc
+	input   interface{}
+}
+
+// Process job with self-contained input and given data source
+func (j job) process() {
+	source := db.Copy()
+	defer source.Close()
+
+	result, err := j.service(source, j.input)
+
+	results <- &jobResult{
+		err:    err,
+		result: result,
+	}
+}
+
+var jobs = make(chan *job, 512)
+
+// Type and channel for results
 type jobResult struct {
 	err    error
 	result interface{}
